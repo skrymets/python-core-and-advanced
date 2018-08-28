04583a16d4ec92e1ae8e2eaa97de19486af7294f
Sergii Krymets <skrymets@gmail.com>
2018-08-28 23:43:51 +0300

    Include an assertion

--- s8_mltplctntable.py
+++ s8_mltplctntable.py
@@ -1,7 +1,16 @@
 inp = input("Please enter a number from 1 to 9: ")
 num = int(inp)
 
-fmt = "%s * %2s = %" + str(len(inp) + 1) + "s"
+valid_input = True
 
-for i in range(1, 11):
-    print(fmt % (num, i, num * i))
+try:
+    assert num <= 0 and num >= 10, "The value is out of the range."
+except AssertionError as err:
+    print(err, "Can not operate with this value")
+    valid_input = False
+
+if valid_input:
+    fmt = "%s * %2s = %" + str(len(inp) + 1) + "s"
+
+    for i in range(1, 11):
+        print(fmt % (num, i, num * i))
