86350b3e04ce2e45c4f6d154c2c6636a58d27065
Sergii Krymets <skrymets@gmail.com>
2018-08-28 23:51:06 +0300

    'continue' and 'break' added

--- s8_mltplctntable.py
+++ s8_mltplctntable.py
@@ -1,16 +1,23 @@
-inp = input("Please enter a number from 1 to 9: ")
-num = int(inp)
+num = 0
 
-valid_input = True
 
-try:
-    assert num <= 0 and num >= 10, "The value is out of the range."
-except AssertionError as err:
-    print(err, "Can not operate with this value")
-    valid_input = False
+def calc():
+    global num
+    
+    while True:
+        inp = input("Please enter a number from 1 to 9: ")
+        num = int(inp)
+        try:
+            assert 0 <= num <= 10, "The value is out of the range."
+            break
+        except AssertionError as err:
+            print(err, "Can not operate with this value")
+            continue
 
-if valid_input:
     fmt = "%s * %2s = %" + str(len(inp) + 1) + "s"
 
     for i in range(1, 11):
         print(fmt % (num, i, num * i))
+
+
+calc()
